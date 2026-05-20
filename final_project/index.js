89d689baab99df2a33326aac4cbfa91ba8d0b242
otmanefihi <otmane.fihi@e-polytechnique.ma>
2026-05-21 00:20:59 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,21 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
+// Configuration de la session pour les clients
+app.use("/customer", session({ 
+  secret: "fingerprint_customer", 
+  resave: true, 
+  saveUninitialized: true 
+}));
 
+// Middleware d'authentification pour sécuriser l'accès aux avis
 app.use("/customer/auth/*", function auth(req, res, next) {
   if (req.session.authorization) {
     let token = req.session.authorization['accessToken'];
+    
     jwt.verify(token, "access", (err, user) => {
       if (!err) {
         req.user = user;
-        next();
+        next(); // L'utilisateur est authentifié, on passe à la route suivante
       } else {
-        return res.status(403).json({ message: "User not authenticated" })
+        return res.status(403).json({ message: "User not authenticated" });
       }
     });
   } else {
-    return res.status(403).json({ message: "User not logged in" })
+    return res.status(403).json({ message: "User not logged in" });
   }
 });
 
